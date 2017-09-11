ea3237aa17cba925cf67a48d38522f5f599b4f9f
Mathias von Kaiz <mathiasvonkaiz@Mathiass-MacBook-Pro-3.local>
2017-09-11 11:33:53 +0200

    code: Fixed version for model.py using generator

--- model.py
+++ model.py
@@ -10,13 +10,12 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 
 
-#lines = []
-samples = []
+lines = []
 correction = 0.2
 with open('./data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
+	print("Process images.")
 	for line in reader:
-#		lines.append(line)
 		for i in range(3):
 			source_path = line[i]
 			filename = source_path.split('/')[-1]
@@ -24,39 +23,25 @@ with open('./data/driving_log.csv') as csvfile:
 			image = cv2.imread(current_path)
 			steering = float(line[3])
 
-			if (i == 1):
+			# add correction for angles on left and right hand side
+			if (i == 0):
 				measurement = steering
-			elif (i == 2):
+			elif (i == 1):
 				measurement = steering + correction
 			else:
 				measurement = steering - correction
 			
-			samples.append([image, measurement])
+			lines.append([image, measurement])
 
 
-#images = []
-#measurements = []
-#for line in lines:
-#	for i in range(3):
-#		source_path = line[i]
-#		filename = source_path.split('/')[-1]
-#		current_path = './data/IMG/' + filename
-#		image = cv2.imread(current_path)
-#		images.append(image)
-#		measurement = float(line[3])
-#		measurements.append(measurement)
-
-#augmented_images, augmented_measurements = [], []
+# Add augmented images for more data
 augmented_samples = []
-for sample in samples:
-#	augmented_images.append(image)
-#	augmented_measurements.append(measurement)
-#	augmented_images.append(cv2.flip(image, 1))
-#	augmented_measurements.append(measurement * -1.0)
+print("Augment images.")
+for sample in lines:
 	augmented_samples.append([sample[0], sample[1]])
 	augmented_samples.append([cv2.flip(sample[0], 1), sample[1] * -1.0])
 
-
+# generator method for returning only needed data
 def generator(samples, batch_size=32):
 	num_samples = len(samples)
 	while 1: # Loop forever so the generator never terminates
@@ -68,25 +53,26 @@ def generator(samples, batch_size=32):
 			angles = []
 			for batch_sample in batch_samples:
 
-				center_image = batch_sample[0] 
-				center_angle = float(batch_sample[1])
-				images.append(center_image)
-				angles.append(center_angle)
+				image = batch_sample[0] 
+				angle = float(batch_sample[1])
+				images.append(image)
+				angles.append(angle)
 			
 				
-
 			X_train = np.array(images)
 			y_train = np.array(angles)
 			yield sklearn.utils.shuffle(X_train, y_train)
 
-#train_samples, validation_samples = train_test_split(np.concatenate(np.array(augmented_images), np.array(augmented_measurements)), test_size=0.2)
+# split data in test and validation
+print("Create test and validation set.")
 train_samples, validation_samples = train_test_split(augmented_samples, test_size=0.2)
 
+# create generators
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
-#X_train = np.array(augmented_images)
-#y_train = np.array(augmented_measurements)
 
+# Create model
+print("Create model.")
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
@@ -95,20 +81,21 @@ model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Convolution2D(64,3,3,activation='relu'))
-#model.add(MaxPooling2D())
-#model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# compile and fit using generator
+print("Start training.")
 model.compile(loss='mse', optimizer='adam')
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 model.fit_generator(train_generator, 
 		    samples_per_epoch=len(train_samples),
 		    validation_data=validation_generator,
 		    nb_val_samples=len(validation_samples), nb_epoch=3)
 
+# save model data
+print("Save model.")
 model.save('model.h5')
 exit()
